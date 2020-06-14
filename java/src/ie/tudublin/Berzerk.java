c02f2b60fefd199f3d70b1982c4746bb574281f3
Danie Di Nardi <c18487682@mytudublin.ie>
2020-06-14 18:35:11 +0100

    gtf out of the way

---
+++ java/src/ie/tudublin/Berzerk.java
@@ -0,0 +1,16 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Berzerk extends PApplet
+{
+    public void settings()
+    {
+        size(500, 500);
+    }
+
+    public void draw()
+    {
+        background(0);
+    }
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -44,6 +44,12 @@ public class Main
         String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new SoundMandala());
     }
+
+    public void berzerk()
+    {
+        String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Berzerk());
+    }
     
     public void dogsCats()
     {
@@ -89,6 +95,6 @@ public class Main
     public static void main(String[] arg)
     {
         Main main = new Main();
-		main.sound1();        
+		main.berzerk();        
     }
 }
